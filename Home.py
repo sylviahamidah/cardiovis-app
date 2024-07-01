05a502588fb3893299b76304276ec17911fbeb94
Sylvia Hamidah <88355419+sylviahamidah@users.noreply.github.com>
2024-07-01 08:23:45 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,33 +5,10 @@ st.set_page_config(
     page_icon="🫀"
 )
 
-st.write("# Welcome to CardioVis! 👋")
-st.markdown('''
-<style>
-.justify-text {
-    text-align: justify;
-}
-</style>
-<div class="justify-text">
-Discover the future of heart health with CardioVis, your ultimate tool for automated cardiomegaly detection using the cardiothoracic ratio. 
-As part of a cutting-edge final project titled <span style="background-color: #287582;">Automation of Cardiothoracic Ratio Calculation from X-Ray Images Segmentation Using Convolutional Neural Networks for Cardiomegaly Detection</span>, 
-this platform harnesses the power of AI to revolutionize heart health diagnostics. 
-Dive into the future of medical technology with us!
-<br><br><br><br>
-</div>
-''', unsafe_allow_html=True)
-
-st.write("##### Meet the Team Behind Cardiovis :eyes:")
-st.markdown('''
-<style>
-.justify-text {
-    text-align: justify;
-}
-</style>
-<div class="justify-text">
-Behind CardioVis are dedicated minds driving innovation in healthcare. 
-<span style="background-color: #307a2d;">Developed by Sylvia Hamidah, guided by Prof. Dr. Tri Arief Sardjono, S.T., M.T., and Nada Fitrieyatul Hikmah, S.T., M.T. </span>, our team combines expertise in medical imaging, artificial intelligence, and healthcare technology. 
-Together, we are committed to advancing cardiac care through cutting-edge research and technology. 
-Join us as we pave the way for a healthier heart future.
-</div>
-''', unsafe_allow_html=True)
+pg = st.navigation([
+    st.Page("About.py", title="About", icon="🏠"),
+    st.Page("Pages\Preprocessing.py", title="Preprocessing", icon="1️⃣"),
+    st.Page("Pages\Segmentation.py", title="Segmentation", icon="2️⃣"),
+    st.Page("Pages\CTR Calculation.py", title="CTR Calculation", icon="3️⃣")
+])
+pg.run()
