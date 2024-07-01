6dcd87ef39601449732437ab269ed876a38ee2bc
Sylvia Hamidah <88355419+sylviahamidah@users.noreply.github.com>
2024-07-01 09:03:00 +0700

    Update Preprocessing.py

--- Pages/Preprocessing.py
+++ Pages/Preprocessing.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-import opencv-python as cv2
+import opencv-python as cv
 import numpy as np
 from skimage.metrics import structural_similarity as compare_ssim
 from contrast_image import quantitation
